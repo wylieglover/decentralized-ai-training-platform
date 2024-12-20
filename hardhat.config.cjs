57420bcaed6aace65acfbeeee196fda88734f2d6
wylieglover <rg11445@georgiasouthern.edu>
2024-12-20 01:08:51 -0500

    update

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
   solidity: "0.8.0",
   networks: {
     sepolia: {
-      url: "https://sepolia.infura.io/v3/58fe1ba825b5413593ff576df1f78809",
+      url: "https://sepolia.infura.io/v3/" + process.env.INFURA_API_KEY,
       accounts: [process.env.PRIVATE_KEY]
     }
   }
